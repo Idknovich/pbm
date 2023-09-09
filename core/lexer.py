2e95785ed94c94ba43a86d8fa5035ce9d1a76eae
Idknovich <nedohacker18338@duck.com>
2023-09-10 01:08:03 +0300

    lexer

--- core/lexer.py
+++ core/lexer.py
@@ -3,7 +3,8 @@ def lex(by: bytes):
     by=by.split(b"\x00")
     fil=[]
     for byte in by:
-        fil.append({"com":byte[0], "params":byte[1:len(by-1)]
+        fil.append({"com":hex(byte[0]), "params":byte[1:]})
     return fil
 
-print(lex(b"\x25\x55\x00\x24"))
+if __name__ == "__main__":
+    print(lex(b"\x55\x13\x00\x55"))
