65de88a378f75aac872b120827d630447da05d03
Idknovich <nedohacker18338@duck.com>
2023-09-10 18:22:55 +0300

    add bytes

--- core/executer/coms.py
+++ core/executer/coms.py
@@ -2,9 +2,11 @@ listt={
     "bytes":{
 	b"\x01":"reg1=arg",
 	b"\x02":"reg2=arg",
-	b"\x03":"reg3=arg"
+	b"\x03":"reg3=arg",
+        b"\x04":"print(reg1)",
+        b"\x05":"print(str(reg1)+str(reg2))"
     }
 }
 
-def get():
-    return listt
+def getb():
+    return listt["bytes"]
