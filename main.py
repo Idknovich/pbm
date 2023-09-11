cd3953a4d8ce803800b0d244912d5874b768536d
Idknovich <nedohacker18338@duck.com>
2023-09-11 09:45:02 +0300

    edited main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def execute(byte, debug=False):
     core.execute(byte, debug)
 
 if __name__=="__main__":
-    program=b"\x03lol\x00\x0613\x00\x04"
+    program=b"\x0252\x00\x0152\x00\x07\x00\x0621\x00\x01Num: \x00\x05"
     execute(program, True)
